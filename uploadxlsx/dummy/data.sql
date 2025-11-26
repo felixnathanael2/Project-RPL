dcf21b40583c8cb82023ae531a84218e41cdb45f
gjsonm <gjsonm@gmail.com>
2025-11-26 11:01:58 +0700

    feat:connect login page with backend, also refactor file placement

--- uploadxlsx/dummy/data.sql
+++
@@ -1,8 +0,0 @@
-CREATE DATABASE ta_bimbingan;
-
-CREATE TABLE jadwal_user (
-    id_jadwal INT AUTO_INCREMENT PRIMARY KEY,
-    jam_mulai TIME NOT NULL,
-    jam_akhir TIME NOT NULL,
-    hari VARCHAR(20) NOT NULL
-);
