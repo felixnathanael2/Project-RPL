f6c69ebcd7113339eae4bf16a0c5b5a6487b66e5
Axel Jeremy <axeljeremy2802@gmail.com>
2025-12-02 20:56:16 +0700

    fix js href modifier agar status required sebelum lanjut

--- frontend/js/scriptTambahAkunAwal.js
+++ frontend/js/scriptTambahAkunAwal.js
@@ -2,7 +2,7 @@ document.getElementById("lanjutButton").onclick = function () {
     let status = document.getElementById("status").value;
     if (status === "Dosen") {
         window.location.href = "TambahPenggunaAdminLanjut_Dosen.html";
-    }else{
+    }else if (status === "Mahasiswa"){
         window.location.href = "TambahPenggunaAdminLanjut_Mahasiswa.html";
     }
 };
